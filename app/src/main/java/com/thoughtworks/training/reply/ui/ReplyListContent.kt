1603988b284be8cf1b0dfee25d91f710de3121af
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-13 14:33:01 +0800

    fix code style

--- app/src/main/java/com/thoughtworks/training/reply/ui/ReplyListContent.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/ReplyListContent.kt
@@ -1,8 +1,6 @@
 package com.thoughtworks.training.reply.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,9 +9,7 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.window.layout.DisplayFeature
