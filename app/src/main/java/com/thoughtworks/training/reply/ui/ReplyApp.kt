1f260a89ee3512f602f8c3ee0489237ecb54d50a
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-13 17:16:25 +0800

    fix selection

--- app/src/main/java/com/thoughtworks/training/reply/ui/ReplyApp.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/ReplyApp.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.window.layout.DisplayFeature
 import com.thoughtworks.training.reply.ui.navigation.ReplyBottomNavigationBar
@@ -59,7 +61,9 @@ private fun ReplyNavigationWrapper(
 ) {
     val navController = rememberNavController()
     val navigationActions = remember(navController) { ReplyNavigationActions(navController) }
-    val selectedDestination = ReplyRoute.INBOX
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    val selectedDestination = navBackStackEntry?.destination?.route ?: ReplyRoute.INBOX
     ReplyAppContent(
         navigationType = navigationType,
         contentType = contentType,
