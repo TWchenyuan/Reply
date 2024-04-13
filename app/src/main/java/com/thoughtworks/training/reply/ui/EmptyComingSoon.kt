df6542f525d909829b6cd92082288de1f3a8fdf4
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-04-13 16:41:14 +0800

    empty comming soon

--- app/src/main/java/com/thoughtworks/training/reply/ui/EmptyComingSoon.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/EmptyComingSoon.kt
@@ -3,11 +3,17 @@ package com.thoughtworks.training.reply.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.thoughtworks.training.reply.R
 
 @Composable
 fun EmptyComingSoon(
@@ -18,7 +24,18 @@ fun EmptyComingSoon(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "empty coming soon")
+        Text(
+            text = stringResource(id = R.string.empty_screen_title),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.empty_screen_subtitle),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
